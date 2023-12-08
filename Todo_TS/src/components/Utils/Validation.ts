2271f6d6cf6b50c7edea5a8351f1978d099ff6b8
SoJuSo <asdfg7123@gmail.com>
2023-12-09 00:41:35 +0900

    [Feat] Validation 파일 추가, 타입 지정 필요

---
+++ Todo_TS/src/components/Utils/Validation.ts
@@ -0,0 +1,11 @@
+const validation = (state) => {
+  state.map((check) => {
+    if (!Object.keys(check).includes("text") || !Object.keys(check).includes("isCompleted")) {
+      console.error(`잘못된 형식으로 저장된 값이 존재합니다!`);
+    } else if (typeof check.text !== "string" || typeof check.isCompleted !== "boolean") {
+      console.error(`${check.text}로 입력된 값이 이상합니다!`);
+    }
+  });
+};
+
+export { validation };
