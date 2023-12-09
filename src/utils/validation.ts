46bfbbcfe044df501b72d48c7aaf899d10dcb6ee
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-12-09 20:56:30 +0900

    remove: class로 바꾸면서 new 연산자 유효성 검사 코드 제거

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,11 +1,6 @@
 import { TodoItem } from "../types/todo.js";
 
 const validation = {
-  newTarget(target) {
-    if (!target) {
-      throw new Error("You must use new keyword");
-    }
-  },
   state(todoList: TodoItem[]) {
     return todoList.filter(
       (todo) =>
