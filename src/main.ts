1c062e514b7beccc1b58d6204e33f957f3aaab5a
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-12-19 01:16:28 +0900

    refactor: 전역 상태 main > App에서 관리하도록 변경

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
 import App from "./App.js";
-import { getItem } from "./utils/storage.js";
-
-const initialState = getItem("todo", []);
-const initialCount = getItem("count", { total: 0, done: 0 });
 const $app = document.querySelector<HTMLElement>("#app");
-
-$app && new App($app, initialState, initialCount);
+$app && new App($app);
